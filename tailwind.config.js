67d6e87de06f70bc718226a089f38495d027fcbe
Hansel Solevilla <sshanzel@yahoo.com>
2020-09-14 16:54:02 +0400

    feat: Tailwind Custom Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+const {colors} = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./src/**/*.html', './src/**/*.js', './src/**/*.jsx'],
   theme: {
     extend: {},
+    colors: {
+      ...colors,
+      primary: '#B8D8BA',
+      secondary: '#D9DBBC',
+      tertiary: '#FCDDBC',
+      quarternary: '#EF9595D',
+      quinary: '#69585F',
+    },
   },
   variants: {},
   plugins: [],
