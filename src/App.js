462e5b81a88c554cce31e2c7be03a17512a867d9
Hansel Solevilla <sshanzel@yahoo.com>
2020-09-14 13:17:06 +0400

    refactor: SignIn

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import {useSelector} from 'react-redux';
 import TodoAppBar from './components/AppBar';
 import './assets/styles/App.css';
 import './assets/styles/main.css';
 import ToDoApp from './view';
 import SignIn from './view/login';
-import {login, register} from './store/actions/userActions';
 
-function App({user, dispatch}) {
+const App = () => {
+  const user = useSelector(state => state.user);
+
+  if (!user.token) return <SignIn />;
+
   return (
     <div className='App'>
       <TodoAppBar user={user} onLogout={() => window.location.reload()} />
       <div className='justify-center px-8'>
-        {user.token ? (
-          <ToDoApp />
-        ) : (
-          <SignIn
-            error={user.error}
-            onLogin={credentials => dispatch(login(credentials))}
-            onRegister={credentials => dispatch(register(credentials))}
-          />
-        )}
+        <ToDoApp />
       </div>
     </div>
   );
-}
+};
 
-const mapStateToProps = state => ({
-  user: state.user,
-});
-
-export default connect(mapStateToProps)(App);
+export default App;
