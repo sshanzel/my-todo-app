34aad1dbe1d15cca558df191ef98447146c28368
Hansel Solevilla <sshanzel@yahoo.com>
2020-09-14 17:49:03 +0400

    refactor: Login Feat

--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -1,32 +1,28 @@
-import http from "../../services/http.service";
-import authService from "../../services/auth.service";
-import { errorToString } from "../../helpers/index";
+import http from '../../services/http.service';
+import authService from '../../services/auth.service';
+import {errorToString} from '../../helpers/index';
 
-export function login(credentials) {
-  return async function(dispatch) {
-    try {
-      const { data } = await authService.login(credentials);
-      http.setJwtHeaderAuth(data);
-      return dispatch({ type: "SET_USER", user: { token: data } });
-    } catch (ex) {
-      return errorToString(ex && ex.response && ex.response.data);
-    }
-  };
-}
+export const login = credentials => async dispatch => {
+  try {
+    const {data} = await authService.login(credentials);
+    http.setJwtHeaderAuth(data);
+    return dispatch({type: 'SET_USER', user: {token: data}});
+  } catch (ex) {
+    return errorToString(ex && ex.response && ex.response.data);
+  }
+};
 
 export function logout() {
   http.setJwtHeaderAuth();
-  return { type: "SET_USER", user: {} };
+  return {type: 'SET_USER', user: {}};
 }
 
-export function register(user) {
-  return async function(dispatch) {
-    try {
-      const { data } = await authService.register(user);
-      http.setJwtHeaderAuth(data.token);
-      return dispatch({ type: "SET_USER", user: { token: data } });
-    } catch (ex) {
-      return errorToString(ex && ex.response && ex.response.data);
-    }
-  };
-}
+export const registerUser = user => async dispatch => {
+  try {
+    const {data} = await authService.register(user);
+    http.setJwtHeaderAuth(data.token);
+    return dispatch({type: 'SET_USER', user: {token: data}});
+  } catch (ex) {
+    return errorToString(ex && ex.response && ex.response.data);
+  }
+};
--- src/view/login.tsx
+++ src/view/login.tsx
@@ -1,22 +1,47 @@
 import React from 'react';
+import {useDispatch} from 'react-redux';
 import Copyright from './Copyright';
 import TSInput from '../components/TSInput';
 import TSButton from '../components/TSButton';
+import {registerUser, login} from '../store/actions/userActions';
 
 import Logo from '../assets/images/the-solevilla.png';
 import TSButtonPlain from '../components/TSButtonPlain';
 
 export const SignIn = () => {
+  const dispatch = useDispatch();
+  const [error, setError] = React.useState('');
+  const [processing, setProcessing] = React.useState(false);
+  const [register, setRegister] = React.useState<string | null>(null);
   const [{username, password}, setCredentials] = React.useState({
     username: '',
     password: '',
   });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLElement>) => {
+    e.preventDefault();
+    setProcessing(true);
+
+    const command: () => any =
+      register !== null
+        ? () => dispatch(registerUser({username, password, name: register}))
+        : () => dispatch(login({username, password}));
+
+    const error = await command();
+
+    if (!error) return;
+
+    setProcessing(false);
+
+    error && setError(error);
+  };
 
   return (
     <div className='flex flex-1 flex-col items-center pt-20'>
       <img alt='Logo' src={Logo} className='w-40' />
       <label className='font-medium text-2xl'>My Pet Projects - Todo App</label>
       <div className='w-full p-4 justify-center lg:w-1/3 md:w-1/2'>
+        {error && <div className='w-full mb-4 flex justify-center text-red-500'>{error}</div>}
         <TSInput
           shadow
           label='Username'
@@ -31,10 +56,26 @@ export const SignIn = () => {
           value={password}
           onInputChange={value => setCredentials(state => ({...state, password: value}))}
         />
-        <TSButton label='Submit' />
+        {register !== null && (
+          <TSInput
+            shadow
+            className='mt-4'
+            label='Name'
+            value={password}
+            onInputChange={value => setRegister(value)}
+          />
+        )}
+        <TSButton label='Submit' onClick={handleSubmit} disabled={processing} />
         <div className='flex flex-row flex-1 justify-between'>
           <TSButtonPlain label='Forgot Password?' />
-          <TSButtonPlain label='Register' />
+          <TSButtonPlain
+            label={
+              register !== null
+                ? 'Have an existing account? Sign-in now!'
+                : `Don't have an account yet? Register now!`
+            }
+            onClick={() => setRegister(register !== null ? null : '')}
+          />
         </div>
         <Copyright />
       </div>
