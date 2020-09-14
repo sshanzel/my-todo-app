7c9dba4a161ba119370613180007caaa088d37be
Hansel Solevilla <sshanzel@yahoo.com>
2020-09-14 14:06:03 +0400

    feat: onInputChange

--- src/components/TSInput.tsx
+++ src/components/TSInput.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
 
-export type TSInputProps = React.HTMLAttributes<HTMLInputElement> & {
+export type TSInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   label?: string;
   value?: string;
+  onInputChange?: (value: string) => void;
 };
 
-const TSInput: React.FC<TSInputProps> = ({label, value, onChange}) => {
+const TSInput: React.FC<TSInputProps> = ({label, value, className, onInputChange, ...props}) => {
   const [text, setText] = React.useState('');
 
+  const handleBlur = React.useCallback(
+    (e: React.FocusEvent<HTMLInputElement>) => {
+      if (!onInputChange) return;
+
+      onInputChange(e.currentTarget.value);
+    },
+    [value]
+  );
+
   React.useEffect(() => {
-    setText(value);
+    setText(value || '');
   }, [value]);
 
   return (
-    <div className='flex flex-col w-full relative'>
+    <div className={`flex flex-col w-full relative ${className}`}>
       <span className='text-xs text-gray-500 absolute -mt-2 ml-4'>{label}:</span>
       <input
-        type='text'
+        {...props}
         value={text}
-        onBlur={onChange}
+        onBlur={handleBlur}
         className='w-full shadow p-2 outline-none'
         onChange={e => setText(e.target.value)}
       />
