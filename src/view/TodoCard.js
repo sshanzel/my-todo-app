dedfb0983de79dfe265d67cd7324596f62d87cd8
Hansel Solevilla <sshanzel@yahoo.com>
2020-09-14 10:59:25 +0400

    feat: truncate

--- src/view/TodoCard.js
+++ src/view/TodoCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const TodoCard = ({title, description, completed}) => {
   return (
-    <div className={`${completed ? 'line-through' : ''} h-20 w-full justify-center`}>
+    <div className={`${completed ? 'line-through' : ''} h-20 w-full justify-center truncate`}>
       <h2 className='text-3xl'>{title}</h2>
       <span className='text-xl'>{description}</span>
     </div>
