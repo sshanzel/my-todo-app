eaaad1bf3b03219a78b2aba6ac1c464776b50311
Hansel Solevilla <sshanzel@yahoo.com>
2020-09-14 16:01:28 +0400

    feat: Optional Border or Shadow

--- src/components/TSInput.tsx
+++ src/components/TSInput.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import clx from 'classnames';
 
 export type TSInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  shadow?: boolean;
+  border?: boolean;
   label?: string;
   value?: string;
   onInputChange?: (value: string) => void;
 };
 
-const TSInput: React.FC<TSInputProps> = ({label, value, className, onInputChange, ...props}) => {
+const TSInput: React.FC<TSInputProps> = ({
+  label,
+  value,
+  className,
+  shadow,
+  border,
+  onInputChange,
+  ...props
+}) => {
   const [text, setText] = React.useState('');
 
   const handleBlur = React.useCallback(
@@ -23,13 +34,13 @@ const TSInput: React.FC<TSInputProps> = ({label, value, className, onInputChange
   }, [value]);
 
   return (
-    <div className={`flex flex-col w-full relative ${className}`}>
+    <div className={clx(`flex flex-col w-full relative`, className)}>
       <span className='text-xs text-gray-500 absolute -mt-2 ml-4'>{label}:</span>
       <input
         {...props}
         value={text}
         onBlur={handleBlur}
-        className='w-full shadow p-2 outline-none'
+        className={clx(`w-full p-2 outline-none text-gray-700`, {shadow, border})}
         onChange={e => setText(e.target.value)}
       />
     </div>
--- src/view/login.tsx
+++ src/view/login.tsx
@@ -16,14 +16,16 @@ export const SignIn = () => {
   return (
     <div className='flex flex-1 flex-col items-center pt-20'>
       <img src={Logo} className='w-40' />
-      <label className='text-gray-600'>Pet Projects - Todo App</label>
+      <label className='font-medium text-2xl'>My Pet Projects - Todo App</label>
       <div className='w-full p-4 justify-center lg:w-1/3 md:w-1/2'>
         <TSInput
+          shadow
           label='Username'
           value={username}
           onInputChange={value => setCredentials(state => ({...state, username: value}))}
         />
         <TSInput
+          shadow
           className='mt-4'
           label='Password'
           type='Password'
