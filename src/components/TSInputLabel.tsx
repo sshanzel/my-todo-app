ff97fbb36a6ca0dc8f5b197a4420bfff31505790
Hansel Solevilla <sshanzel@yahoo.com>
2020-09-15 14:45:43 +0400

    refactor: Input Label

--- src/components/TSInputLabel.tsx
+++ src/components/TSInputLabel.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 export interface TSInputLabelProps {}
 
 const TSInputLabel: React.FC<TSInputLabelProps> = ({children}) => {
-  return <span className='text-xs text-gray-500 absolute -mt-2 ml-4'>{children}</span>;
+  return (
+    <span className='text-xs text-gray-500 absolute -mt-2 ml-4 bg-white px-1'>{children}</span>
+  );
 };
 
-export default TSInputLabel;
+export default React.memo(TSInputLabel);
