b476a9c1d5e4c721464ecf0da01416c4fed7efcb
Hansel Solevilla <sshanzel@yahoo.com>
2020-09-15 16:48:49 +0400

    refactor: TSButtonPlain

--- src/components/TSButtonPlain.tsx
+++ src/components/TSButtonPlain.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import clx from 'classnames';
 
-export type TSButtonPlainProps = React.ButtonHTMLAttributes<HTMLElement> & {};
+export type TSButtonPlainProps = React.ButtonHTMLAttributes<HTMLElement> & {
+  contentClassName?: string;
+};
 
-const TSButtonPlain: React.FC<TSButtonPlainProps> = ({children, ...rest}) => {
+const TSButtonPlain: React.FC<TSButtonPlainProps> = ({
+  children,
+  className,
+  contentClassName,
+  ...rest
+}) => {
   return (
-    <button className='focus:outline-none' {...rest}>
-      <span className='text-sm text-gray-600'>{children}</span>
+    <button className={clx('focus:outline-none', className)} {...rest}>
+      <span className={clx('text-sm', contentClassName)}>{children}</span>
     </button>
   );
 };
