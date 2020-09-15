312de1d6a4b06f0ada5f7391eaf9669397235907
Hansel Solevilla <sshanzel@yahoo.com>
2020-09-15 15:42:01 +0400

    refactor: TodoForm

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import TodoList from './TodoList';
-import TodoForm from './TodoForm';
+import TodoItem from './TodoItem';
 import {RootState} from '../store/reducers';
 import AddTodo from '../components/AddTodo';
 import SimpleModal from '../components/Modal';
+import todosService from '../services/todos.service';
 import {initTodo, Todo} from '../store/reducers/types';
 import {retrieveTodos} from '../store/actions/todoActions';
+import {updateTodo, deleteTodo, createTodo} from '../store/actions/todoActions';
 
 export interface TodoAppProps {}
 
@@ -19,6 +21,34 @@ const TodoApp: React.FC<TodoAppProps> = () => {
     dispatch(retrieveTodos());
   }, [dispatch]);
 
+  const handleChange = (value: string | Date, field: keyof Todo) => {
+    if (!todo) return;
+
+    setTodo({...todo, [field]: value});
+  };
+
+  const handleSave = () => {
+    if (!todo) return;
+
+    const {error} = todosService.validate(todo);
+
+    if (error) return;
+
+    const command = todo._id ? updateTodo : createTodo;
+
+    dispatch(command(todo));
+
+    setTodo(undefined);
+  };
+
+  const handleDelete = () => {
+    setTodo(undefined);
+
+    if (!todo || !todo._id) return;
+
+    dispatch(deleteTodo(todo));
+  };
+
   return (
     <div className='flex flex-1 flex-col mt-4'>
       <div className='mb-4'>
@@ -26,7 +56,7 @@ const TodoApp: React.FC<TodoAppProps> = () => {
       </div>
       <TodoList todos={todos} onClick={(t: Todo) => setTodo(t)} />
       <SimpleModal open={!!todo} onClose={() => setTodo(undefined)}>
-        <TodoForm todo={todo} sideEffect={() => setTodo(undefined)} />
+        <TodoItem todo={todo} onSave={handleSave} onDelete={handleDelete} onChange={handleChange} />
       </SimpleModal>
     </div>
   );
